21e53ae0fe29fae3c7b0068bdad1ba8216bbf4bd
coyee1 <121971846+coyee1@users.noreply.github.com>
2023-03-07 14:31:14 -0800

    4.3

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,8 +17,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     Vector3 lookDirection = (player.transform.position-transform.position).normalized;
-     
+        Vector3 lookDirection = (player.transform.position-transform.position).normalized;   
         enemyRb.AddForce(lookDirection * speed);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    public bool hasPowerup;
     
     // Start is called before the first frame update
     void Start()
@@ -20,5 +21,20 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis ("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Power-up"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy")&& hasPowerup)
+        {
+            Debug.Log("Collided with " + collision.gameObject.name + "with powerup set to " + hasPowerup);
+        }
     }
 }
